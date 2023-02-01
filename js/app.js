857cae6634fed661c5d26578aae3a2ebe8eb7c13
albertopiffer <albertopiffer96@gmail.com>
2023-02-01 19:44:59 +0100

    FEAT milestone 2

--- js/app.js
+++ js/app.js
@@ -2,20 +2,40 @@ const formButton = document.querySelector('#formIn input[type="button"]')
 
 formButton.addEventListener('click', function () {
 
-const km = document.querySelector('#formIn input[name="nKm"]')
+    const km = document.querySelector('#formIn input[name="nKm"]')
+    const nome = document.querySelector('#formIn input[name="nomCog"]')
 
-const scontoStringa = document.querySelector('#formIn select[name="sconto"]')
+    const scontoStringa = document.querySelector('#formIn select[name="sconto"]')
 
-console.log(scontoStringa.value)
+    console.log(scontoStringa.value)
 
-let sconto
-let prezzoKm = parseFloat (0.21)
+    let sconto
+    let prezzoKm = parseFloat (0.21)
 
-if (scontoStringa.value == 'u18') {  sconto = parseFloat (0.2) }
-if (scontoStringa.value == 'o65') { sconto = parseFloat (0.4) }
-if (scontoStringa.value == 'ns') { sconto = parseFloat (1) }
+    if (scontoStringa.value == 'u18') {  sconto = parseFloat (0.2) }
+    if (scontoStringa.value == 'o65') { sconto = parseFloat (0.4) }
+    if (scontoStringa.value == 'ns') { sconto = parseFloat (1) }
 
-let prezzo = (1 - sconto) * parseFloat (km.value) * prezzoKm
-console.log(prezzo)
+    let prezzo = parseFloat (km.value) * prezzoKm
+    let prezzosc = prezzo * (1 - sconto)
+    console.log(prezzo)
+
+
+    document.getElementById("scontrino").style.display = "block"
+
+    const jsKm = document.getElementById('sKm')
+    jsKm.innerHTML = km.value
+
+    const jsNome = document.getElementById('sNome')
+    jsNome.innerHTML = nome.value
+
+    const jsPrezzons = document.getElementById('sPrezzons')
+    jsPrezzons.innerHTML = prezzo.toFixed(2)
+
+    const jsSconto = document.getElementById('sSconto')
+    jsSconto.innerHTML = sconto * 100
+
+    const jsPrezzo = document.getElementById('sPrezzo')
+    jsPrezzo.innerHTML = prezzosc.toFixed(2)
 
 });
